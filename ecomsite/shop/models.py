c3e6260d8381dff7dc2a44695e170cb8d065ec74
RohiniPunde <punderohini@gmail.com>
2020-09-22 01:12:47 -0700

    Added shopping cart with counts and place order.

--- ecomsite/shop/models.py
+++ ecomsite/shop/models.py
@@ -11,3 +11,16 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+class Order(models.Model):
+    items=models.CharField(max_length=50)
+    name=models.CharField(max_length=50)
+    email=models.CharField(max_length=50)
+    address=models.CharField(max_length=50)
+    city=models.CharField(max_length=50)
+    state=models.CharField(max_length=50)
+    zipcode=models.CharField(max_length=50)
+    total=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
